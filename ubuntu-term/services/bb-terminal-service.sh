8c45fa1424cff32873429f35d2422926829583e3
Alper Reha Yazgan <alperreha@gmail.com>
2024-07-19 13:10:52 +0300

    bb-terminal-service workdir updated.

--- ubuntu-term/services/bb-terminal-service.sh
+++ ubuntu-term/services/bb-terminal-service.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
+# set terminal and bash defaults
+export TERM=xterm-256color
+
+# set workspace
+cd /root/workspace
+
+# start terminal
 while true; do /usr/bin/bb-terminal -p 40020 -m 64 --writable -t disableLeaveAlert=true bash; done

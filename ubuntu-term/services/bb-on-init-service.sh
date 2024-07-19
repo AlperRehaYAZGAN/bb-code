52727e949042cf92f11323d2202c4b03b2c9c340
Alper Reha Yazgan <alperreha@gmail.com>
2024-07-19 14:28:12 +0300

    [ubuntu-term]: bb-terminal-service and bb-on-init-helper services injected.

---
+++ ubuntu-term/services/bb-on-init-service.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# start terminal
+echo "[INFO] OnInit - Starting Terminal" > /tmp/bb-on-init-service.log
+echo "[INFO] OnInit - bb-terminal service starting..." >> /tmp/bb-on-init-service.log
+echo "[INFO] OnInit - Completed" >> /tmp/bb-on-init-service.log
--- ubuntu-term/services/bb-terminal-service.sh
+++ ubuntu-term/services/bb-terminal-service.sh
@@ -3,8 +3,5 @@
 # set terminal and bash defaults
 export TERM=xterm-256color
 
-# set workspace
-cd /root/workspace
-
 # start terminal
 while true; do /usr/bin/bb-terminal -p 40020 -m 64 --writable -t disableLeaveAlert=true bash; done

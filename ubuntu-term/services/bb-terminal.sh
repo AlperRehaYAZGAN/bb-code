aed928b7e86b788043ffcb85237c81700f1d520f
Alper Reha Yazgan <alperreha@gmail.com>
2024-07-21 04:47:57 +0300

    [ubuntu-term]: service filenames updated.

--- ubuntu-term/services/bb-terminal.sh
+++ ubuntu-term/services/bb-terminal.sh
@@ -4,7 +4,7 @@
 export TERM=xterm-256color
 
 # set working directory /root/workspace
-cd /root/workspace
+# cd /root/workspace
 
 # start terminal
 while true; do /usr/bin/bb-terminal -p 40020 -m 64 --writable -t disableLeaveAlert=true bash; done

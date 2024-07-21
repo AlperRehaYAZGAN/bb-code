ce997fab34d3e126423b6df382852bd2f6840bf5
Alper Reha Yazgan <alperreha@gmail.com>
2024-07-22 02:05:57 +0300

    [ubuntu-term]: image.env activated.

--- ubuntu-term/services/bb-terminal.sh
+++ ubuntu-term/services/bb-terminal.sh
@@ -20,6 +20,7 @@ unset ISENVSUCCESS
 # load init-script from repository
 # if repository returns non-200 code. Use default : "bb-terminal -p 40020 -m 64 --writable -t disableLeaveAlert=true /bin/bash;"
 # curl -s https://gitlab.bulutbilisimciler.com/bb-public/playground-init/-/raw/main/bb-terminal/bb-terminal.sh?ref_type=heads | /bin/bash
+export $(grep -v '^#' /tmp/image.env | xargs)
 
 URL="https://gitlab.bulutbilisimciler.com/bb-public/playground-init/-/raw/main/bb-terminal/bb-terminal.sh?ref_type=heads"
 ISSUCCESS=$(curl -s -o /dev/null -w "%{http_code}" $URL)

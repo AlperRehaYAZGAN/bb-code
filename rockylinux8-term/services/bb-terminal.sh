4866ca4f9ec7c42c23d72d945bd100e44cff0df6
Alper Reha Yazgan <alperreha@gmail.com>
2024-07-21 17:32:13 +0300

    [rockylinux8-term]: systemd does not support env but we add it on service booting with image.env script added.

--- rockylinux8-term/services/bb-terminal.sh
+++ rockylinux8-term/services/bb-terminal.sh
@@ -5,6 +5,8 @@ export TERM=xterm-256color
 
 # set working directory /root/workspace
 # cd /root/workspace
+export $(grep -v '^#' /tmp/env/image.env | xargs)
+export $(grep -v '^#' /tmp/env/bb.env | xargs)
 
 # start terminal with nohup
 nohup /usr/bin/bb-terminal -p 40020 -m 64 --writable -t disableLeaveAlert=true bash

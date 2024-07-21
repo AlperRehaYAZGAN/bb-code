8d742b9e9aa60bfe2440f00e895bbcd67b4dd2af
Alper Reha Yazgan <alperreha@gmail.com>
2024-07-21 18:39:01 +0300

    [all-terms]: all bb-on-init and bb-terminal scripts updated.

--- alpine-term/services/bb-on-init.sh
+++ alpine-term/services/bb-on-init.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 
 # start terminal
-echo "[INFO] OnInit - Starting Terminal" > /tmp/bb-on-init-service.log
-echo "[INFO] OnInit - bb-terminal service starting..." >> /tmp/bb-on-init-service.log
+echo "[INFO] OnInit - ENV Loading additional environment variables" > /tmp/bb-on-init-service.log
+
+ENV_URL="https://gitlab.bulutbilisimciler.com/bb-public/playground-init/-/raw/main/bb-on-init/additional.env"
+ISSUCCESS=$(curl -s -o /dev/null -w "%{http_code}" $URL)
+if [ $ISSUCCESS -eq 200 ]; then
+    curl -s $ENV_URL > /tmp/additional.env
+    source /tmp/additional.env
+fi
 
 # if "INIT_SCRIPT_URL" given, download and run it
 if [ -n "$INIT_SCRIPT_URL" ]; then
-  echo "[INFO] OnInit - Downloading and running INIT_SCRIPT_URL: $INIT_SCRIPT_URL" >> /tmp/bb-on-init-service.log
+  echo "[INFO] OnInit - INIT_SCRIPT_URL is downloading... $INIT_SCRIPT_URL" >> /tmp/bb-on-init-service.log
   curl -s $INIT_SCRIPT_URL | bash
   echo "[INFO] OnInit - INIT_SCRIPT_URL completed" >> /tmp/bb-on-init-service.log
 fi
--- alpine-term/services/bb-terminal.sh
+++ alpine-term/services/bb-terminal.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 
 # load init-script from repository
-curl -s https://gitlab.bulutbilisimciler.com/bb-public/playground-init/-/raw/main/bb-terminal/bb-terminal.sh?ref_type=heads | bash
+# if repository returns non-200 code. Use default : "/usr/bin/bb-terminal -p 40020 -m 64 --writable -t disableLeaveAlert=true bash;"
+# curl -s https://gitlab.bulutbilisimciler.com/bb-public/playground-init/-/raw/main/bb-terminal/bb-terminal.sh?ref_type=heads | bash
+
+URL="https://gitlab.bulutbilisimciler.com/bb-public/playground-init/-/raw/main/bb-terminal/bb-terminal.sh?ref_type=heads"
+ISSUCCESS=$(curl -s -o /dev/null -w "%{http_code}" $URL)
+if [ $ISSUCCESS -eq 200 ]; then
+    curl -s $URL | bash
+else
+    /usr/bin/bb-terminal -p 40020 -m 64 --writable -t disableLeaveAlert=true bash;
+fi

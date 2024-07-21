b94119b4103db8a58273c712a98e396a76d797bc
Alper Reha Yazgan <alperreha@gmail.com>
2024-07-21 17:16:04 +0300

    [rockylinux8-term]: unnecessary ddocs removed and nohup in service file added.

--- rockylinux8-term/services/bb-terminal.sh
+++ rockylinux8-term/services/bb-terminal.sh
@@ -6,5 +6,5 @@ export TERM=xterm-256color
 # set working directory /root/workspace
 # cd /root/workspace
 
-# start terminal
-/usr/bin/bb-terminal -p 40020 -m 64 --writable -t disableLeaveAlert=true bash
+# start terminal with nohup
+nohup /usr/bin/bb-terminal -p 40020 -m 64 --writable -t disableLeaveAlert=true bash &

38226c2464fd67ed3ef225d5ab486d0130f67269
Alper Reha Yazgan <alperreha@gmail.com>
2024-07-25 15:32:54 +0300

    [pardus-term]: pardus boilerpalte updated.

---
+++ pardus-term/services/bb-on-init.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+echo "[BB-ONINIT] OnInit - Script has started." > /tmp/bb-on-init-service.log
+
+ENV_URL="https://gitlab.bulutbilisimciler.com/bb-public/playground-init/-/raw/main/bb-on-init/additional.env"
+ISENVSUCCESS=$(curl -s -o /dev/null -w "%{http_code}" $ENV_URL)
+if [ $ISENVSUCCESS -eq 200 ]; then
+  echo "[BB-ONINIT] ENV - envs are loading..." >> /tmp/bb-on-init-service.log
+  curl -s $ENV_URL > /tmp/on-init-additional.env
+  export $(grep -v '^#' /tmp/on-init-additional.env | xargs)
+  echo "[BB-ONINIT] ENV - envs loaded" >> /tmp/bb-on-init-service.log
+else 
+    echo "[BB-ONINIT] ENV - envs are not found." >> /tmp/bb-on-init-service.log
+fi
+
+# if "INIT_SCRIPT_URL" given, download and run it
+if [ -n "$INIT_SCRIPT_URL" ]; then
+  echo "[BB-ONINIT] OnInit - INIT_SCRIPT_URL env found set and try downloading... $INIT_SCRIPT_URL" >> /tmp/bb-on-init-service.log
+  curl -s $INIT_SCRIPT_URL | /bin/bash
+  echo "[BB-ONINIT] OnInit - INIT_SCRIPT_URL completed" >> /tmp/bb-on-init-service.log
+fi
+
+echo "[BB-ONINIT] OnInit - Script Run Completed" >> /tmp/bb-on-init-service.log
+
+# in the end create /tmp/ready file to indicate that service is ready
+touch /tmp/ready
---
+++ pardus-term/services/bb-terminal.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+echo "[BB-TERMINAL] OnInit - ENV Loading from remote additional environment variables repository" > /tmp/bb-terminal-service.log
+
+ENV_URL="https://gitlab.bulutbilisimciler.com/bb-public/playground-init/-/raw/main/bb-terminal/additional.env"
+ISENVSUCCESS=$(curl -s -o /dev/null -w "%{http_code}" $ENV_URL)
+if [ $ISENVSUCCESS -eq 200 ]; then
+    echo "[BB-TERMINAL] ENV - envs are loading..." >> /tmp/bb-terminal-service.log
+    curl -s $ENV_URL > /tmp/terminal-additional.env
+    export $(grep -v '^#' /tmp/terminal-additional.env | xargs)
+    echo "[BB-TERMINAL] ENV - envs loaded" >> /tmp/bb-terminal-service.log
+else 
+    echo "[BB-TERMINAL] ENV - envs are not found." >> /tmp/bb-terminal-service.log
+fi
+
+# unset
+unset ENV_URL
+unset ISENVSUCCESS
+
+# load init-script from repository
+# if repository returns non-200 code. Use default : "bb-terminal -p 40020 -m 64 --writable -t disableLeaveAlert=true /bin/bash;"
+# curl -s https://gitlab.bulutbilisimciler.com/bb-public/playground-init/-/raw/main/bb-terminal/bb-terminal.sh?ref_type=heads | /bin/bash
+
+URL="https://gitlab.bulutbilisimciler.com/bb-public/playground-init/-/raw/main/bb-terminal/bb-terminal.sh?ref_type=heads"
+ISSUCCESS=$(curl -s -o /dev/null -w "%{http_code}" $URL)
+if [ $ISSUCCESS -eq 200 ]; then
+    echo "[BB-TERMINAL] SCRIPT - Script is downloading..." >> /tmp/bb-terminal-service.log
+    curl -s $URL | /bin/bash
+    echo "[BB-TERMINAL] SCRIPT - Script completed" >> /tmp/bb-terminal-service.log
+else
+    # unset
+    unset URL
+    unset ISSUCCESS
+
+    echo "[BB-TERMINAL] SCRIPT - Script is not found. Using default script." >> /tmp/bb-terminal-service.log
+    bb-terminal -p 40020 -m 64 --writable -t disableLeaveAlert=true /bin/bash;
+fi
